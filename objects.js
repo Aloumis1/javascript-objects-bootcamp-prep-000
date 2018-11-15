bfe9dc6dd815a041e94a30ad83a2ec63b30f895f
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-15 03:33:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,16 +1,10 @@
-it('defines an object called `playlist`', () => {
-    expect(typeof playlist).to.equal('object')
-  })
-   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      expect(updatePlaylist({}, 'Phil Ochs', "Here's to the State of Mississippi")).
-        to.eql({ 'Phil Ochs': "Here's to the State of Mississippi" })
-    })
-  })
-   describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      expect(removeFromPlaylist({ Kanye: "Gold Digger" }, "Kanye")).
-        to.eql({})
-    })
-  })
-})
+var playlist = new Object({keys: '0'});
+
+ function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
